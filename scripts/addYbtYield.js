71da3c488b20ee53c770628de44011cb9bf48a36
Bhimgouda D Patil <satvikp508@gmail.com>
2024-11-28 13:06:44 +0530

    minor change

--- scripts/addYbtYield.js
+++ scripts/addYbtYield.js
@@ -5,26 +5,26 @@ const { abi } = require("../abi/stETH.mock.sol/StETH.json");
 const ybtAbi = abi;
 
 async function addYbtYield(wallet, ybts) {
-  try {
-    let currentNonce = await wallet.getNonce();
+  let currentNonce = await wallet.getNonce();
 
-    ybts.map((ybt) => {
-      const address = ybt.address;
+  ybts.map((ybt) => {
+    const address = ybt.address;
 
-      const contract = new ethers.Contract(address, ybtAbi, wallet);
-      contract.addInterest({ nonce: currentNonce });
+    const contract = new ethers.Contract(address, ybtAbi, wallet);
+    contract.addInterest({ nonce: currentNonce });
 
-      currentNonce++;
-    });
-  } catch (error) {
-    console.log(error);
-  }
+    currentNonce++;
+  });
 }
 
 async function scheduleAddYbtYieldCronjob(wallet, ybts) {
   cron.schedule("*/5 * * * *", () => {
     console.log("Adding Interest to the YBTs");
-    addYbtYield(wallet, ybts);
+    try {
+      addYbtYield(wallet, ybts);
+    } catch (error) {
+      console.log(error, "Error adding YBT Yield");
+    }
   });
 }
 
